8f11b3a8aae35f7d43aa8cbbdb3c3e77a68fb677
Simran Kalirai <kaliraisimran@gmail.com>
2022-08-31 20:39:09 +0000

    created an input file for the input functions

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 // establishes a connection with the game server
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  console.log(key);
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+
 console.log("Connecting ...");
 connect();
 setupInput();
